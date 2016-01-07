546b62156c38b046b5bb4433e36336a72d9a5f12
Taran Ostenson <tlostenson@gmail.com>
2016-01-06 17:33:30 -0700

    added screenshots

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 angular.module('meApp', ['ngMaterial'])
 
 angular.module('meApp')
-	.controller('meController', ['$scope'], function($scope){
-		
-	})
+	.controller('meController', ['$scope', function(meController, $scope){
+
+	}])
