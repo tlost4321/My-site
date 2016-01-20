73cc12044bc9c2a22beb1c450e354cae9429a625
Taran Ostenson <tlostenson@gmail.com>
2016-01-20 16:10:08 -0700

    info added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/public'));
 
 // Routes \\
 app.get('/', function(req, res){
-  res.send('Hello')
+  res.sendFile('../html/index.html')
 });
 
 // Creating Server and Listening for Connections \\
