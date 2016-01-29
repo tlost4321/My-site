f88a718422714f52d0b767d56f6be20ffff7264e
Taran Ostenson <tlostenson@gmail.com>
2016-01-29 12:09:09 -0700

    styling changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
 angular.module('meApp', ['ngMaterial'])
-
+	.config(function($mdThemingProvider) {
+  		$mdThemingProvider.theme('default')
+    		.primaryPalette('blue-grey', {
+    			'default': '500',
+    			'hue-1'  : '100',
+    			'hue-2'  : '300',
+    		})
+    .accentPalette('green',{
+    	'default': '500'
+    });
+});
 angular.module('meApp')
 	.controller('meController', ['$scope', function(meController, $scope){
 
