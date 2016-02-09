56f96c6259a85e18748ab8ab3b7749451f769779
Taran Ostenson <tlostenson@gmail.com>
2016-02-09 13:58:39 -0700

    lots done

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,36 @@ angular.module('meApp', ['ngMaterial'])
    });
 });
 angular.module('meApp')
-	.controller('meController', ['$scope', function(meController, $scope){
+    .factory('techFactory', function(){
+
+        var techList = []
+
+        var Tech = function(name, type, image){
+            this.name = name;
+            this.type = type;
+            this.image = image;
+
+            techList.push(this)
+        }
+
+        new Tech('HTML 5', 'Front-End', '../images/tags/html5.png')
+        new Tech('CSS3', 'Front-End', "../images/tags/css3.jpg")
+        new Tech('Bootstrap', 'Front-End', '../images/tags/bootstrap.png')
+        new Tech('JavaScript', 'Front and Back-End', '../images/tags/js.png')
+        new Tech('AngularJS', 'Front-End', '../images/tags/angularjs.png')
+        new Tech('NodeJS', 'Back-End', '../images/tags/nodejs.png')
+        new Tech('MongoDB', 'Back-End', '../images/tags/mongodb.png')
+        new Tech('SQL', 'Back-End', '../images/tags/sql.png')
+
+        return {
+            Tech : Tech,
+            techList : techList
+        }
+    })
+angular.module('meApp')
+	.controller('meController', ['$scope', 'techFactory', function($scope, techFactory){
+
+        $scope.techs = techFactory.techList
+
 
 	}])
