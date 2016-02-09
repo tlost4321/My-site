361c943ee5118ad78c3ae8d606da142e9e055c44
Taran Ostenson <tlostenson@gmail.com>
2016-02-09 11:35:41 -0700

    pics

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
 angular.module('meApp', ['ngMaterial'])
 	.config(function($mdThemingProvider) {
   		$mdThemingProvider.theme('default')
-    		.primaryPalette('blue-grey', {
-    			'default': '500',
-    			'hue-1'  : '100',
-    			'hue-2'  : '300',
-    		})
+            .dark('grey',{
+
+            })
+    		
+
+    			// 'default': '500',
+    			// 'hue-1'  : '100',
+    			// 'hue-2'  : '300',
+    		
     .accentPalette('green',{
-    	'default': '500'
-    });
+    	'default': '200'
+   });
 });
 angular.module('meApp')
 	.controller('meController', ['$scope', function(meController, $scope){
